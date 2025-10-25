4c9c557705b828adcc0b2663c4312e03e5e47ef4
Siyabonga Samson Lukhele <samsonlukhele76@gmail.com>
2025-10-25 10:21:39 +0200

    feat: add live alert functionality with dynamic alert messages

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,34 @@
 import "./style.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
+
+const alertContainer = document.querySelector(".alertcontainer") as HTMLElement;
+const alertButton = document.getElementById(
+  "live-alert-button"
+) as HTMLButtonElement;
+
+type AlertType =
+  | "primary"
+  | "secondary"
+  | "success"
+  | "danger"
+  | "warning"
+  | "info"
+  | "light"
+  | "dark";
+
+const alert = (message: string, type: AlertType) => {
+  const wrapper = document.createElement("div");
+
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible fade show" role="alert">`,
+    `<div>${message}</div>`,
+    `<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>`,
+    `</div>`,
+  ].join("");
+
+  alertContainer.append(wrapper);
+};
+
+alertButton.addEventListener("click", () => {
+  alert("This is a live alert!", "info");
+});
